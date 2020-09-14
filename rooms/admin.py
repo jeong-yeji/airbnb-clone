5b5cefef10fcd50657e889cb9378574b51bcf70f
jeong-yeji <jeongyeji.mail@gmail.com>
2020-09-15 02:59:35 +0900

    #6 Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -17,12 +17,73 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "address",
+                    "price",
+                    "room_type",
+                )
+            },
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),  # 접을 수 있는 속성
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Deatils", {"fields": ("host",)}),
+    )  # 보여지는 필트 설정
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    # 우선순위 정해서 정렬해서 보여주기
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")  # 다대다 관계에서 작동하는 필터
+
+    def count_amenities(self, obj):  # self: RoomAdmin, obj: current row
+        print(obj.amenities.all())
 
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """ """
+    """ Photo Admin Definition """
 
     pass
