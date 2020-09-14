99fc47957fc72ec40a59dcd805c40df67dbf0869
jeong-yeji <jeongyeji.mail@gmail.com>
2020-09-14 21:15:59 +0900

    #4. Room App

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from core import models as core_models
 from django_countries.fields import CountryField
-from users import models as user_models
+
+# from users import models as user_models
 
 # Create your models here.
 
@@ -21,7 +22,48 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
 
-    pass
+    """ RoomType Model Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    # python은 위에서부터 코드를 읽기 때문에 "" 없이 쓰려면 class Room 아래에 있어야 함
+
+    def __str__(self):
+        return self.caption
 
 
 # room이 언제 등록, 업데이트 됐는지 확인하는 기능 만들고 싶음
@@ -45,8 +87,14 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)  # 일대다관계
-    room_type = models.ManyToManyField(RoomType, blank=True)  # 다대다관계
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)  # 일대다관계
+    # on_delete=models.CASCADE : host 삭제하면 host에 포함 된 room도 삭제됨
+    room_type = models.ForeignKey(
+        RoomType, on_delete=models.SET_NULL, null=True
+    )  # 다대다관계
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
