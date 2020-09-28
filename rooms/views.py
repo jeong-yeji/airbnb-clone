4a7f952fa2f5cd1c38128d2330420142715b7b63
jeong-yeji <jeongyeji.mail@gmail.com>
2020-09-29 03:43:06 +0900

    #10.0-10.1 Urls, Views, HttpResponse and render

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def all_rooms(request):
+    now = datetime.now()
+    return HttpResponse(content=f"<h1>{now}</h1>")
