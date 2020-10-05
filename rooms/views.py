64c8acc87a49968957b724dccb689b09a0fbf377
jeong-yeji <jeongyeji.mail@gmail.com>
2020-10-05 11:49:01 +0900

    #11.7-11.8 Class Based Views

--- rooms/views.py
+++ rooms/views.py
@@ -1,35 +1,21 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
-def all_rooms(request):
-    """ python 코드 이용
-    page = request.GET.get("page", 1) # request가 없는 경우 > default 값 1
-    page = int(page or 1) # page값이 비워져있는 경우 오류 발생 > default값으로 1 주기
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(request, "rooms/home.html",
-        {
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(0, page_count),
-        },
-    )
-    """
-    
-    # Django Paginator
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5) 
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_date(self, **kwargs):
+        context = super().get_context_date(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
