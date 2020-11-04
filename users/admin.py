909f12d22a820160167346ae10eb201f2649b510
jeong-yeji <jeongyeji.mail@gmail.com>
2020-11-04 15:54:00 +0900

    #17 Log in with Github

--- users/admin.py
+++ users/admin.py
@@ -31,6 +31,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -51,4 +52,5 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "email_verified",
         "email_secret",
+        "login_method",
     )
