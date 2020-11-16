b108b12016fb6497c7cdd4b4c90af3aab132aa29
jeong-yeji <jeongyeji.mail@gmail.com>
2020-11-16 21:59:20 +0900

    #21.10 Great User Mixins

--- users/mixins.py
+++ users/mixins.py
@@ -1,6 +1,18 @@
 from django.contrib import messages
+from django.urls import reverse_lazy
 from django.shortcuts import redirect
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
+
+class EmailLoginOnlyView(UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user.login_method == "email"
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
+        return redirect("core:home")
+
 
 class LoggedOutOnlyView(UserPassesTestMixin):
 
@@ -12,3 +24,9 @@ class LoggedOutOnlyView(UserPassesTestMixin):
     def handle_no_permission(self):
         messages.error(self.request, "Can't go there")
         return redirect("core:home")
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+
+    login_url = reverse_lazy("users:login")
+
